c5bcd77c00e000150e381afbfacf382a8fd628c5
HuanNguyenARL <huann@nevada.unr.edu>
2020-06-21 08:47:59 -0700

    Add acc command converter

---
+++ rotors_control/src/nodes/acc_command_converter.cpp
@@ -0,0 +1,101 @@
+#include "acc_command_converter.h"
+
+#include "rotors_control/parameters_ros.h"
+
+namespace rotors_control {
+
+AccCommandConverterNode::AccCommandConverterNode() 
+  : receive_first_odom(false)
+{
+  ros::NodeHandle nh;
+
+  cmd_rate_thrust_sub_ = nh.subscribe("command/rate_thrust", 1,
+                                     &AccCommandConverterNode::RateThrustCallback, this);
+
+  cmd_roll_pitch_yawrate_thrust_pub_ = nh.advertise<mav_msgs::RollPitchYawrateThrust>(
+      kDefaultCommandRollPitchYawrateThrustTopic, 1);
+
+  ros::NodeHandle pnh("~");
+  GetRosParameter(pnh, "use_vehicle_frame", true, &use_vehicle_frame); 
+  GetRosParameter(pnh, "mass", 1.0, &mass); 
+  ROS_WARN_STREAM("Mass:" << mass);
+  if (!use_vehicle_frame) // need to know current yaw angle of the robot if acc vector is expressed in world frame
+  {
+    odometry_sub_ = nh.subscribe(kDefaultOdometryTopic, 1, &AccCommandConverterNode::OdometryCallback, this);
+  }
+}
+
+AccCommandConverterNode::~AccCommandConverterNode() { }
+
+void AccCommandConverterNode::RateThrustCallback(
+    const mav_msgs::RateThrustPtr& rate_thrust_msg) {
+  if ((!use_vehicle_frame) && (!receive_first_odom))
+  {
+    return;
+  }
+  mav_msgs::RollPitchYawrateThrustPtr rpyrate_thrust_cmd(new mav_msgs::RollPitchYawrateThrust);
+  Eigen::Vector3d current_rpy;
+  odometry.getEulerAngles(&current_rpy);
+  // double current_roll = current_rpy(0);
+  // double current_pitch = current_rpy(1);
+  double current_yaw = (use_vehicle_frame)?0.0:current_rpy(2);
+  Eigen::Vector3d thrust_sp;
+  thrust_sp << rate_thrust_msg->thrust.x, rate_thrust_msg->thrust.y, rate_thrust_msg->thrust.z;
+  thrust_sp = (thrust_sp + Eigen::Vector3d(0, 0, kGravity)) * mass;
+  Eigen::Vector3d thrust_norm = thrust_sp.normalized();
+
+  double cphi_stheta = thrust_norm(0) * cos(current_yaw) + thrust_norm(1) * sin(current_yaw);
+  double sphi = thrust_norm(0) * sin(current_yaw) - thrust_norm(1) * cos(current_yaw);
+  double cphi_ctheta = thrust_norm(2);
+  if (cphi_ctheta != 0)
+  {
+    rpyrate_thrust_cmd->pitch = atan2(cphi_stheta, cphi_ctheta);
+    rpyrate_thrust_cmd->roll = atan2(sphi, sqrt(cphi_stheta * cphi_stheta + cphi_ctheta * cphi_ctheta));
+  }
+  else
+  {
+    rpyrate_thrust_cmd->pitch = 0;
+    rpyrate_thrust_cmd->roll = 0;
+  }
+
+  rpyrate_thrust_cmd->yaw_rate = rate_thrust_msg->angular_rates.z; // calculate from 
+  //#endif
+  rpyrate_thrust_cmd->thrust.x = 0;
+  rpyrate_thrust_cmd->thrust.y = 0;
+  // total thrust is the norm of thrust vector
+  rpyrate_thrust_cmd->thrust.z = thrust_sp.norm();
+  
+  // OR cross-product of thrust vector and current z_B axis
+  //rpyrate_thrust_cmd->thrust.z = thrust_sp(0)*(cos(current_roll)*sin(current_pitch)*cos(current_yaw) + sin(current_roll)*sin(current_yaw))
+  //                           + thrust_sp(1)*(cos(current_roll)*sin(current_pitch)*sin(current_yaw) - sin(current_roll)*cos(current_yaw))
+  //                           + thrust_sp(2)*cos(current_roll)*cos(current_pitch);
+
+  rpyrate_thrust_cmd->header.frame_id = frame_id;
+  rpyrate_thrust_cmd->header.stamp = ros::Time::now();
+
+  cmd_roll_pitch_yawrate_thrust_pub_.publish(rpyrate_thrust_cmd);
+}
+
+
+void AccCommandConverterNode::OdometryCallback(const nav_msgs::OdometryConstPtr& odometry_msg) {
+
+  ROS_INFO_ONCE("AccCommandConverter node got first odometry message.");
+  if (!receive_first_odom)
+  {
+    frame_id = odometry_msg->header.frame_id; 
+    receive_first_odom = true;
+  }
+  mav_msgs::eigenOdometryFromMsg(*odometry_msg, &odometry); 
+}
+
+}
+
+int main(int argc, char** argv) {
+  ros::init(argc, argv, "acc_command_converter");
+
+  rotors_control::AccCommandConverterNode acc_command_converter_node;
+
+  ros::spin();
+
+  return 0;
+}
---
+++ rotors_control/src/nodes/acc_command_converter.h
@@ -0,0 +1,47 @@
+#ifndef ROTORS_CONTROL_ROLL_PITCH_YAWRATE_THRUST_CONTROLLER_NODE_H
+#define ROTORS_CONTROL_ROLL_PITCH_YAWRATE_THRUST_CONTROLLER_NODE_H
+
+#include <boost/bind.hpp>
+#include <Eigen/Eigen>
+#include <stdio.h>
+
+#include <geometry_msgs/PoseStamped.h>
+#include <mav_msgs/RollPitchYawrateThrust.h>
+#include <mav_msgs/RateThrust.h>
+#include <nav_msgs/Odometry.h>
+#include <ros/ros.h>
+#include <ros/callback_queue.h>
+
+#include "rotors_control/common.h"
+#include "rotors_control/roll_pitch_yawrate_thrust_controller.h"
+
+namespace rotors_control {
+
+class AccCommandConverterNode {
+ public:
+  AccCommandConverterNode();
+  ~AccCommandConverterNode();
+
+ private:
+  static constexpr double kGravity = 9.8066;
+
+  bool use_vehicle_frame;
+  bool receive_first_odom;
+  mav_msgs::EigenOdometry odometry;
+  std::string frame_id;
+
+  double mass;
+
+  // subscribers
+  ros::Subscriber cmd_rate_thrust_sub_;
+  ros::Subscriber odometry_sub_;
+
+  ros::Publisher cmd_roll_pitch_yawrate_thrust_pub_;
+
+  void RateThrustCallback(const mav_msgs::RateThrustPtr& rate_thrust_msg);
+
+  void OdometryCallback(const nav_msgs::OdometryConstPtr& odometry_msg);
+};
+}
+
+#endif // ROTORS_CONTROL_ROLL_PITCH_YAWRATE_THRUST_CONTROLLER_NODE_H
